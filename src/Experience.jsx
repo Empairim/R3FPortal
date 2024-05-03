6b1c8ff321ce4c5f0b357b21f5ecf04df903ec20
Empairim <titusfaulknertull@yahoo.com>
2024-05-02 22:23:09 -0400

    added baked models in

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,14 +1,22 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
 
 export default function Experience()
 {
+    const {nodes} = useGLTF("./model/portal.glb")
+    const bakedTexture = useTexture("./model/baked.jpg")
+    
+    bakedTexture.flipY = false
+    //upside down fix
     return <>
 
+        <color args={ ["black"] } attach="background" />
+       
         <OrbitControls makeDefault />
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
+        <mesh 
+         geometry={ nodes.baked.geometry }
+        >
+            <meshBasicMaterial map={ bakedTexture } />
         </mesh>
 
     </>
